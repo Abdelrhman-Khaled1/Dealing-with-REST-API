a9bfffe16e754bc556c722c1920be68c37ffea10
Abdelrhman <kabdo17013@gmail.com>
2024-01-21 12:50:37 +0200

    validate create user

--- src/main/java/com/abdelrhman/social/media/Exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/abdelrhman/social/media/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.abdelrhman.social.media.Exception;
 
 import com.abdelrhman.social.media.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -27,5 +31,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+                "Total Errors: "+ex.getErrorCount() +" First Error: "+ex.getFieldError(),
+                request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+//        return this.handleExceptionInternal(ex, (Object)null, headers, status, request);
+    }
+
 
 }
